b87778fbe40df2474110e974e3df8fb2503c1a74
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2017-09-14 15:13:18 +0900

    fixed installing packages

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -2,6 +2,8 @@
 
 #install packages
 sudo apt-get install ros-indigo-cv-bridge
+sudo apt-get install ros-indigo-perception-pcl
+sudo apt-get install ros-indigo-turtlebot-gazebo
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/ros_tutorial_pkgs_20170907
