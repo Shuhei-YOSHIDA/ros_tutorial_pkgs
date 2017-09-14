bae5c25df63b38b8a1803ad17836925c1d9305be
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2017-09-14 15:21:18 +0900

    add tf2-geometry-msgs install

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -4,6 +4,7 @@
 sudo apt-get install ros-indigo-cv-bridge
 sudo apt-get install ros-indigo-perception-pcl
 sudo apt-get install ros-indigo-turtlebot-gazebo
+sudo apt-get install ros-indigo-tf2-geometry-msgs
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/ros_tutorial_pkgs_20170907
