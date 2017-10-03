6f516c8f1ec0c7a3fe8930daab20d7239587abd8
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2017-10-04 00:21:20 +0900

    fixed travis test file

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -7,6 +7,6 @@ sudo apt-get install ros-indigo-turtlebot-gazebo
 sudo apt-get install ros-indigo-tf2-geometry-msgs
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/ros_tutorial_pkgs_20170907
+rsync -av ./ ~/catkin_ws/src/ros_tutorial_pkgs
 cd ~/catkin_ws
 catkin_make
